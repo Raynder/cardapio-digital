3b0daad181199cd6df829b52986618e73d1e3b83
Raynder Lobe <Raynder4@gmail.com>
2022-05-26 21:56:49 -0300

    feat: iniciando desenvolvimento

--- resources/views/app/produtos/index.blade.php
+++ resources/views/app/produtos/index.blade.php
@@ -4,9 +4,9 @@
     <header>
         <nav>
             <div class="flex action-bar">
-                <a href="page-index.html"><span class="las la-angle-left"></span></a>
+                <a href="{{ route('app') }}"><span class="las la-angle-left"></span></a>
                 <div class="info">
-                    <h2>Empresas</h2>
+                    <h2>Mais pedidos</h2>
                 </div>
             </div>
         </nav>
@@ -19,57 +19,57 @@
             <div class="category-item">
                 <a href="products.html">
                     <div class="item-img" style="background-image: url('{{asset('img/app/pizzariaCardoso.jpg') }}')"></div>
-                    <h4>Pizzaria Cardoso</h4>
+                    <h4>Hamburguer 1</h4>
                 </a>
             </div>
             <div class="category-item">
                 <a href="products.html">
                     <div class="item-img" style="background-image: url('{{asset('img/app/pizzariaCardoso.jpg') }}')"></div>
-                    <h4>Pizzaria Cardoso</h4>
+                    <h4>Hamburguer 2</h4>
                 </a>
             </div>
             <div class="category-item">
                 <a href="products.html">
                     <div class="item-img" style="background-image: url('{{asset('img/app/pizzariaCardoso.jpg') }}')"></div>
-                    <h4>Pizzaria Cardoso</h4>
+                    <h4>Hamburguer 3</h4>
                 </a>
             </div>
             <div class="category-item">
                 <a href="products.html">
                     <div class="item-img" style="background-image: url('{{asset('img/app/pizzariaCardoso.jpg') }}')"></div>
-                    <h4>Pizzaria Cardoso</h4>
+                    <h4>Hamburguer 4</h4>
                 </a>
             </div>
             <div class="category-item">
                 <a href="products.html">
                     <div class="item-img" style="background-image: url('{{asset('img/app/pizzariaCardoso.jpg') }}')"></div>
-                    <h4>Pizzaria Cardoso</h4>
+                    <h4>Hamburguer 5</h4>
                 </a>
             </div>
             <div class="category-item">
                 <a href="products.html">
                     <div class="item-img" style="background-image: url('{{asset('img/app/pizzariaCardoso.jpg') }}')"></div>
-                    <h4>Pizzaria Cardoso</h4>
+                    <h4>Hamburguer 6</h4>
                 </a>
             </div>
             <div class="category-item">
                 <a href="products.html">
                     <div class="item-img" style="background-image: url('{{asset('img/app/pizzariaCardoso.jpg') }}')"></div>
-                    <h4>Pizzaria Cardoso</h4>
+                    <h4>Hamburguer 7</h4>
                 </a>
             </div>
             <div class="category-item">
                 <a href="products.html">
                     <div class="item-img" style="background-image: url('{{asset('img/app/pizzariaCardoso.jpg') }}')"></div>
-                    <h4>Pizzaria Cardoso</h4>
+                    <h4>Hamburguer 8</h4>
                 </a>
             </div>
             <div class="category-item">
                 <a href="products.html">
                     <div class="item-img" style="background-image: url('{{asset('img/app/pizzariaCardoso.jpg') }}')"></div>
-                    <h4>Pizzaria Cardoso</h4>
+                    <h4>Hamburguer 9</h4>
                 </a>
             </div>
         </div>
     </div>
-@endsection
+@endsection  
